dae77e2bc2521bfd0828dedaceccb71e0d5ed3be
Roberto L. de Oliveira Jr <rloliveirajr@gmail.com>
2013-01-24 20:44:36 -0200

    creating project

---
+++ src/br/ufmg/dcc/probs/Main.java
@@ -0,0 +1,13 @@
+package br.ufmg.dcc.probs;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/br/ufmg/dcc/probs/NaivePathGeneration.java
@@ -0,0 +1,5 @@
+package br.ufmg.dcc.probs;
+
+public class NaivePathGeneration {
+
+}
